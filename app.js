5fd69a682539213f92adbb2eba2176fd83f11db1
Sohomcn <sohom.paul@capitalnumbers.com>
2022-01-29 00:39:02 +0530

    mvc

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const app = express();
 const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
 
-app.set('view engine', 'pug');
+const app = express();
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public'))); //declare asset path
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-app.use('/admin', adminRoutes.routes);
 
 app.use((req, res, next) => {
-    //res.status(404).send('<h1>404 Not found</h1>');
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
