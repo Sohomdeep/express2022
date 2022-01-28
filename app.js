304d66375c9f9c6b7c0f848cbcfd671e94c7081d
Sohomcn <sohom.paul@capitalnumbers.com>
2022-01-28 17:57:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))); //declare asset path
+
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+
+app.use((req, res, next) => {
+    //res.status(404).send('<h1>404 Not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
