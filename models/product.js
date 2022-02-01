7118174f2977d2589a138bd186d0e8a8281346ef
Sohomcn <sohom.paul@capitalnumbers.com>
2022-02-01 13:01:51 +0530

    data save

--- models/product.js
+++ models/product.js
@@ -1,12 +1,29 @@
 const products = [];
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
     save() {
-        products.push(this);
+        const p = path.join(
+            require.main.filename, 
+               '../', 'data', 
+                'products.json'
+            );
+            fs.readFile(p, (err, fileContent) => {
+                let products = [];
+                if(!err){
+                    products = JSON.parse(fileContent);
+                }
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err);
+                });
+            });
     }
 
     static fetchAll() {
