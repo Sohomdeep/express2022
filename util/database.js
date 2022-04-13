67aadbb10f91766918fde69a8e845abf19784403
Sohomcn <sohom.paul@capitalnumbers.com>
2022-04-13 23:53:33 +0530

    product find with id, product details with mysql query

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+    database: 'express',
+    password: '123'
 });
 
 module.exports = pool.promise();
