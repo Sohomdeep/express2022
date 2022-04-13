a54f527e39bbb6741aed0b542db20980809490b6
Sohomcn <sohom.paul@capitalnumbers.com>
2022-04-13 23:43:15 +0530

    fetch product with mysql query

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'express',
-    password: '123'
+    database: 'node-complete',
+    password: 'nodecomplete'
 });
 
 module.exports = pool.promise();
