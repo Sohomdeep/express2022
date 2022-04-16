93a907416588320ad67484bdf35cb854c4b65017
Sohomcn <sohom.paul@capitalnumbers.com>
2022-04-16 10:23:19 +0530

    fetch product with _id using mongo

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://expresssd:cn@12345SD#@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://sddev:7pYaRGkvkc5dfTwk@cluster0.1ucc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
